908426b27576f63bd83a5aff4e48b423d3c128d8
Yurii Matusevich <matusevichy@gmail.com>
2023-09-20 16:35:37 +0300

    Changed abonent and organization pages

--- resources/views/abonent/show.blade.php
+++ resources/views/abonent/show.blade.php
@@ -5,156 +5,133 @@
 @section('content')
     <div class="container">
         @include('notice.index_abon', $notices)
-        <div class="row">
-            <div class="col">
-                {{__('Account number')}}
+        <div class="d-flex align-items-start">
+            <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <button class="nav-link active" id="v-pills-general-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-general" type="button" role="tab" aria-controls="v-pills-general"
+                        aria-selected="true">{{__('General')}}
+                </button>
+                <button class="nav-link" id="v-pills-tarifs-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-tarifs" type="button" role="tab" aria-controls="v-pills-tarifs"
+                        aria-selected="false">{{__('Current tarifs')}}
+                </button>
+                <button class="nav-link" id="v-pills-saldo-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-saldo" type="button" role="tab"
+                        aria-controls="v-pills-saldo"
+                        aria-selected="false">{{__('Saldo')}}
+                </button>
+                <button class="nav-link" id="v-pills-accruals-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-accruals" type="button" role="tab" aria-controls="v-pills-accruals"
+                        aria-selected="false">{{__('Accruals')}}
+                </button>
+                <button class="nav-link" id="v-pills-payments-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-payments" type="button" role="tab" aria-controls="v-pills-payments"
+                        aria-selected="false">{{__('Payments')}}
+                </button>
+                <button class="nav-link" id="v-pills-counters-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-counters" type="button" role="tab" aria-controls="v-pills-counters"
+                        aria-selected="false">{{__('Counters')}}
+                </button>
             </div>
-            <div class="col">
-                {{$abonent->account_number}}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                {{__('Location number')}}
-            </div>
-            <div class="col">
-                {{$abonent->location_number}}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                {{__('Street')}}
-            </div>
-            <div class="col">
-                {{$abonent->street}}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                {{__("Square")}},{{__('m')}}<sup>2</sup>
-            </div>
-            <div class="col">
-                {{$abonent->square}}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                {{__('Owner')}}
-            </div>
-            <div class="col">
-                {{$abonent->user->name}} {{$abonent->user->phone}}
-            </div>
-        </div>
-        <div class="row  justify-content-center">
-            <div class="col-4 text-center">
-                {{__('Arrears')}}
-            </div>
-        </div>
-        @foreach($accrual_types as $accrual_type)
-            @php($current_saldo = get_abonent_saldo_by_accrual_type($abonent->id, $accrual_type->id))
-            @if($current_saldo !== 0)
-                <div class="row">
-                    <div class="col">
-                        {{__($accrual_type->name)}}  {{($accrual_type->by_counter == true)? "(".__("by counter").")":""}}
+            <div class="tab-content container" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="v-pills-general" role="tabpanel"
+                     aria-labelledby="v-pills-home-tab">
+                    <div class="row">
+                        <div class="col">
+                            {{__('Account number')}}
+                        </div>
+                        <div class="col">
+                            {{$abonent->account_number}}
+                        </div>
                     </div>
-                    <div class="col">
-                        {{$current_saldo}}
+                    <div class="row">
+                        <div class="col">
+                            {{__('Location number')}}
+                        </div>
+                        <div class="col">
+                            {{$abonent->location_number}}
+                        </div>
                     </div>
+                    <div class="row">
+                        <div class="col">
+                            {{__('Street')}}
+                        </div>
+                        <div class="col">
+                            {{$abonent->street}}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            {{__("Square")}},{{__('m')}}<sup>2</sup>
+                        </div>
+                        <div class="col">
+                            {{$abonent->square}}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            {{__('Owner')}}
+                        </div>
+                        <div class="col">
+                            {{$abonent->user->name}} {{$abonent->user->phone}}
+                        </div>
+                    </div>
+                    <div class="row  justify-content-center">
+                        <div class="col-4 text-center">
+                            {{__('Arrears')}}
+                        </div>
+                    </div>
+                    @foreach($accrual_types as $accrual_type)
+                        @php($current_saldo = get_abonent_saldo_by_accrual_type($abonent->id, $accrual_type->id))
+                        @if($current_saldo !== 0)
+                            <div class="row">
+                                <div class="col">
+                                    {{__($accrual_type->name)}}  {{($accrual_type->by_counter == true)? "(".__("by counter").")":""}}
+                                </div>
+                                <div class="col">
+                                    {{$current_saldo}}
+                                </div>
+                            </div>
+                        @endif
+                    @endforeach
                 </div>
-            @endif
-        @endforeach
-
-        <div class="accordion" id="accordion_abonent">
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="tarifs_header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#tarifs" aria-expanded="false" aria-controls="tarifs">
-                        {{__('Current tarifs')}}
-                    </button>
-                </h2>
-                <div id="tarifs" class="accordion-collapse collapse" aria-labelledby="tarifs_header"
-                     data-bs-parent="#accordion_abonent">
-                    <div class="accordion-body">
-                        <table class="table table-striped table-responsive-md">
-                            <thead>
+                <div class="tab-pane fade" id="v-pills-tarifs" role="tabpanel" aria-labelledby="v-pills-tarifs-tab">
+                    <table class="table table-striped table-responsive-md">
+                        <thead>
+                        <tr>
+                            <th scope="col">{{__('Accrual type')}}</th>
+                            <th>{{__('Value')}}</th>
+                            <th>{{__('Date begin')}}</th>
+                            <th>{{__('Date end')}}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($tarifs as $tarif)
                             <tr>
-                                <th scope="col">{{__('Accrual type')}}</th>
-                                <th>{{__('Value')}}</th>
-                                <th>{{__('Date begin')}}</th>
-                                <th>{{__('Date end')}}</th>
+                                <td>{{__($tarif->accrualtype->name)}}</td>
+                                <td>{{$tarif->value}}</td>
+                                <td>{{$tarif->date_begin}}</td>
+                                <td>{{$tarif->date_end}}</td>
                             </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($tarifs as $tarif)
-                                <tr>
-                                    <td>{{__($tarif->accrualtype->name)}}</td>
-                                    <td>{{$tarif->value}}</td>
-                                    <td>{{$tarif->date_begin}}</td>
-                                    <td>{{$tarif->date_end}}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="saldo_header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#saldo" aria-expanded="false" aria-controls="saldo">
-                        {{__('Saldo')}}
-                    </button>
-                </h2>
-                <div id="saldo" class="accordion-collapse collapse" aria-labelledby="saldo_header"
-                     data-bs-parent="#accordion_abonent">
-                    <div class="accordion-body">
-                        @include('saldo.index', $saldos)
-                    </div>
+                <div class="tab-pane fade" id="v-pills-saldo" role="tabpanel" aria-labelledby="v-pills-saldo-tab">
+                    @include('saldo.index', $saldos)
                 </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="accruals_header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#accruals" aria-expanded="false" aria-controls="accruals">
-                        {{__('Accruals')}}
-                    </button>
-                </h2>
-                <div id="accruals" class="accordion-collapse collapse" aria-labelledby="accruals_header"
-                     data-bs-parent="#accordion_abonent">
-                    <div class="accordion-body">
-                        @include('accrual.index', $accruals)
-                    </div>
+                <div class="tab-pane fade" id="v-pills-accruals" role="tabpanel" aria-labelledby="v-pills-accruals-tab">
+                    @include('accrual.index', $accruals)
                 </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="payments_header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#payments" aria-expanded="false" aria-controls="payments">
-                        {{__('Payments')}}
-                    </button>
-                </h2>
-                <div id="payments" class="accordion-collapse collapse" aria-labelledby="payments_header"
-                     data-bs-parent="#accordion_abonent">
-                    <div class="accordion-body">
-                        @include('payment.index', $payments)
-                    </div>
+                <div class="tab-pane fade" id="v-pills-payments" role="tabpanel" aria-labelledby="v-pills-payments-tab">
+                    @include('payment.index', $payments)
                 </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="counters_header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#counters" aria-expanded="false" aria-controls="counters">
-                        {{__('Counters')}}
-                    </button>
-                </h2>
-                <div id="counters" class="accordion-collapse collapse" aria-labelledby="counters_header"
-                     data-bs-parent="#accordion_abonent">
-                    <div class="accordion-body">
-                        @include('counter.index', $counters)
-                    </div>
+                <div class="tab-pane fade" id="v-pills-counters" role="tabpanel" aria-labelledby="v-pills-counters-tab">
+                    @include('counter.index', $counters)
                 </div>
             </div>
         </div>
+
     </div>
 
 @endsection
--- resources/views/organization/show.blade.php
+++ resources/views/organization/show.blade.php
@@ -4,105 +4,87 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col">
-                {{__(('Organization name'))}}
+        <div class="d-flex align-items-start">
+            <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <button class="nav-link active" id="v-pills-general-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-general" type="button" role="tab" aria-controls="v-pills-general"
+                        aria-selected="true">{{__('General')}}
+                </button>
+                <button class="nav-link" id="v-pills-notice_for_owners-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-notice_for_owners" type="button" role="tab"
+                        aria-controls="v-pills-notice_for_owners"
+                        aria-selected="false">{{__('Notices for owner')}}
+                </button>
+                <button class="nav-link" id="v-pills-notices-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-notices" type="button" role="tab" aria-controls="v-pills-notices"
+                        aria-selected="false">{{__('Notices')}}
+                </button>
+                <button class="nav-link" id="v-pills-tarifs-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-tarifs" type="button" role="tab" aria-controls="v-pills-tarifs"
+                        aria-selected="false">{{__('Tarifs')}}
+                </button>
+                <button class="nav-link" id="v-pills-abonents-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-abonents" type="button" role="tab" aria-controls="v-pills-abonents"
+                        aria-selected="false">{{__('Abonents')}}
+                </button>
             </div>
-            <div class="col">
-                {{$organization->name}}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                {{__(('Location'))}}
-            </div>
-            <div class="col">
-                {{$organization->location}}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                {{__(('Abonents limit'))}}
-            </div>
-            <div class="col">
-                {{$organization->user_limit}}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                {{__(('Accrual date'))}}
-            </div>
-            <div class="col">
-                {{$organization->accrual_date}}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                {{__('Owners')}}
-            </div>
-            <div class="col">
-                @foreach($organization->users as $user)
-                    {{$user->name}}
-                @endforeach
-            </div>
-        </div>
-        <div class="accordion" id="accordion_organization">
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="notice_for_owners_header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#notice_for_owners" aria-expanded="false" aria-controls="notice_for_owners">
-                        {{__('Notices for owner')}}
-                    </button>
-                </h2>
-                <div id="notice_for_owners" class="accordion-collapse collapse" aria-labelledby="notice_for_owners_header"
-                     data-bs-parent="#accordion_organization">
-                    <div class="accordion-body">
-                        @include('noticeforowner.index', $organization)
+            <div class="tab-content container" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="v-pills-general" role="tabpanel"
+                     aria-labelledby="v-pills-home-tab">
+                    <div class="row">
+                        <div class="col">
+                            {{__(('Organization name'))}}
+                        </div>
+                        <div class="col">
+                            {{$organization->name}}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            {{__(('Location'))}}
+                        </div>
+                        <div class="col">
+                            {{$organization->location}}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            {{__(('Abonents limit'))}}
+                        </div>
+                        <div class="col">
+                            {{$organization->user_limit}}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            {{__(('Accrual date'))}}
+                        </div>
+                        <div class="col">
+                            {{$organization->accrual_date}}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            {{__('Owners')}}
+                        </div>
+                        <div class="col">
+                            @foreach($organization->users as $user)
+                                {{$user->name}}
+                            @endforeach
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="notices_header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#notices" aria-expanded="false" aria-controls="notices">
-                        {{__('Notices')}}
-                    </button>
-                </h2>
-                <div id="notices" class="accordion-collapse collapse" aria-labelledby="notices_header"
-                     data-bs-parent="#accordion_organization">
-                    <div class="accordion-body">
-                        @include('notice.index', $organization)
-                    </div>
+                <div class="tab-pane fade" id="v-pills-notice_for_owners" role="tabpanel" aria-labelledby="v-pills-notice_for_owners-tab">
+                    @include('noticeforowner.index', $organization)
                 </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="tarifs_header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#tarifs" aria-expanded="false" aria-controls="tarifs">
-                        {{__('Tarifs')}}
-                    </button>
-                </h2>
-                <div id="tarifs" class="accordion-collapse collapse" aria-labelledby="tarifs_header"
-                     data-bs-parent="#accordion_organization">
-                    <div class="accordion-body">
-                        <a href="{{route('accruals.createByOrg', $organization->id)}}" class="btn btn-primary  mb-1"
-                           role="button">{{__("Create accruals for abonents")}} </a>
-                        @include('tarif.index', $organization)
-                    </div>
+                <div class="tab-pane fade" id="v-pills-notices" role="tabpanel" aria-labelledby="v-pills-notices-tab">
+                    @include('notice.index', $organization)
                 </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="abonents_header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#abonents" aria-expanded="false" aria-controls="abonents">
-                        {{__('Abonents')}}
-                    </button>
-                </h2>
-                <div id="abonents" class="accordion-collapse collapse" aria-labelledby="abonents_header"
-                     data-bs-parent="#accordion_organization">
-                    <div class="accordion-body">
-                        @include('abonent.index', [$organization, $abonents])
-                    </div>
+                <div class="tab-pane fade" id="v-pills-tarifs" role="tabpanel" aria-labelledby="v-pills-tarifs-tab">
+                    @include('tarif.index', $organization)
+                </div>
+                <div class="tab-pane fade" id="v-pills-abonents" role="tabpanel" aria-labelledby="v-pills-abonents-tab">
+                    @include('abonent.index', [$organization, $abonents])
                 </div>
             </div>
         </div>
